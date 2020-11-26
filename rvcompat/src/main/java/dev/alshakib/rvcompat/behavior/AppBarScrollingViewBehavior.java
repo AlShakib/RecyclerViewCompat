8953d878f22d49e76458a532e0aac06c57a792da
AlShakib <shakib@alshakib.dev>
2020-11-26 23:47:03 +0600

    Add AppBarScrollingViewBehavior

---
+++ rvcompat/src/main/java/dev/alshakib/rvcompat/behavior/AppBarScrollingViewBehavior.java
@@ -0,0 +1,75 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Al Shakib (shakib@alshakib.dev)
+ *
+ * This file is part of Recycler View Compat
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package dev.alshakib.rvcompat.behavior;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+public class AppBarScrollingViewBehavior extends AppBarLayout.ScrollingViewBehavior {
+
+    private AppBarLayout appBarLayout;
+
+    public AppBarScrollingViewBehavior() {
+        super();
+    }
+
+    public AppBarScrollingViewBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onDependentViewChanged(@NonNull CoordinatorLayout parent, @NonNull View child,
+                                          @NonNull View dependency) {
+        if (appBarLayout == null) {
+            appBarLayout = (AppBarLayout) dependency;
+        }
+
+        final boolean result = super.onDependentViewChanged(parent, child, dependency);
+        final int bottomPadding = calculateBottomPadding(appBarLayout);
+        if (bottomPadding != child.getPaddingBottom()) {
+            child.setPadding(
+                    child.getPaddingLeft(),
+                    child.getPaddingTop(),
+                    child.getPaddingRight(),
+                    bottomPadding);
+            child.requestLayout();
+        }
+        return bottomPadding != child.getPaddingBottom() || result;
+    }
+
+    private int calculateBottomPadding(AppBarLayout dependency) {
+        final int totalScrollRange = dependency.getTotalScrollRange();
+        return totalScrollRange + dependency.getTop();
+    }
+}
